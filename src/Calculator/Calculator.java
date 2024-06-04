56c5f95a638b93e3de9e1302ebb73ff834d79eec
Gary Smith <garysmith193358@gmail.com>
2024-06-03 21:25:34 -0400

    added earned values to hashmap, displayed earned values and total sales

--- src/Calculator/Calculator.java
+++ src/Calculator/Calculator.java
@@ -3,19 +3,25 @@ import java.util.HashMap;
 
 public class Calculator {
     public static void main(String[] args) {
-        HashMap<String, String> prices = new HashMap<String, String>();
+        HashMap<String, Number[]> prices = new HashMap<>();
 
-        prices.put("Bubblegum", "$2");
-        prices.put("Toffee", "$0.2");
-        prices.put("Ice cream", "$5");
-        prices.put("Milk chocolate", "$4");
-        prices.put("Doughnut", "$2.5");
-        prices.put("Pancake", "$3.2");
+        prices.put("Bubblegum", new Number[]{2, 202});
+        prices.put("Toffee", new Number[]{0.2, 118});
+        prices.put("Ice cream", new Number[]{5, 2250});
+        prices.put("Milk chocolate", new Number[]{4, 1680});
+        prices.put("Doughnut", new Number[]{2.5, 1075});
+        prices.put("Pancake", new Number[]{3.2, 80});
 
-        System.out.println("Prices:");
+        int totalSales = 0;
+
+        System.out.println("Earned amount:");
         for (String product: prices.keySet()) {
-            System.out.println(product + ": " + prices.get(product));
+            int earnedValue = prices.get(product)[1].intValue();
+
+            totalSales = totalSales + earnedValue;
+            System.out.println(product + ": $" + earnedValue);
         }
 
+        System.out.println("Income: $" + totalSales);
     }
 }
